11c2f25b91a0574e6062cded64057db280f876a1
manticorevault <artur_seilah@hotmail.com>
2021-07-13 15:55:52 -0300

    Handled the authentication on Google. Everything working on the redirect to google profil

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ const keys = require("./config/keys");
 const app = express();
 
 // Configure the passport to use the Google Strategy with the imported keys
-passport.use(new GoogleStrategy({ 
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback"
- },
-    (accessToken) => {
+passport.use(new GoogleStrategy(
+    { 
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken) => {
         console.log(accessToken);
     })
 );
 
+// Handle user authentication on passport and google.
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
+}));
+
 // Pass the port the app identifies as the active one
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 5000;
 
 // Log the port where it's running 
 app.listen(PORT, () => {
