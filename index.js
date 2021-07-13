19647411b5087fcb3ef9e97489e5c0ee347cb76c
manticorevault <artur_seilah@hotmail.com>
2021-07-13 15:32:38 -0300

    Updated the passport and google strategy to handle the imported keys

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 // Bring in the express app
 const app = express();
 
-// Configure the passport to use the Google Strategy;
-passport.use(new GoogleStrategy());
+// Configure the passport to use the Google Strategy with the imported keys
+passport.use(new GoogleStrategy({ 
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+ },
+    (accessToken) => {
+        console.log(accessToken);
+    })
+);
 
 // Pass the port the app identifies as the active one
 const PORT = process.env.PORT || 3333;
