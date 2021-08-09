b81b5ca41f4b0ee0bcfd6e2a2b3a40e43bbf377f
manticorevault <artur_seilah@hotmail.com>
2021-08-09 17:23:15 -0300

    Refactored the App.js, updated the application with a proxy service and added a setupProxy.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Functioning.
+          Functional App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/auth/google"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sign in with Google
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
